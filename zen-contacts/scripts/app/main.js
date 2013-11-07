99e1d64d651fa27d97e36764a243eb25f1a5a37c
Alex Gyoshev <katheron+hackathon@gmail.com>
2013-11-07 12:26:47 -0600

    Upgrade Kendo to LIB, styles cleanup, use flat theme

--- zen-contacts/scripts/app/main.js
+++ zen-contacts/scripts/app/main.js
@@ -1,37 +1,38 @@
-    'use strict';
+'use strict';
 
-    // global error handling
-    var showAlert = function(message, title, callback) {
-        navigator.notification.alert(message, callback || function () {
-        }, title, 'OK');
-    };
-    var showError = function(message) {
-        showAlert(message, 'Error occured');
-    };
-    window.addEventListener('error', function (e) {
-        e.preventDefault();
-        var message = e.message + "' from " + e.filename + ":" + e.lineno;
-        showAlert(message, 'Error occured');
-        return true;
-    });
+// global error handling
+var showAlert = function(message, title, callback) {
+    navigator.notification.alert(message, callback || function () {
+    }, title, 'OK');
+};
+var showError = function(message) {
+    showAlert(message, 'Error occured');
+};
+window.addEventListener('error', function (e) {
+    e.preventDefault();
+    var message = e.message + "' from " + e.filename + ":" + e.lineno;
+    showAlert(message, 'Error occured');
+    return true;
+});
 
-    var onBackKeyDown = function(e) {
-        e.preventDefault();
-        navigator.notification.confirm('Do you really want to exit?', function (confirmed) {
-            var exit = function () {
-                navigator.app.exitApp();
-            };
-            if (confirmed === true || confirmed === 1) {
-                AppHelper.logout().then(exit, exit);
-            }
-        }, 'Exit', 'Ok,Cancel');
-    };
-    var onDeviceReady = function() {
-        //Handle document events
-        document.addEventListener("backbutton", onBackKeyDown, false);
-    };
+var onBackKeyDown = function(e) {
+    e.preventDefault();
+    navigator.notification.confirm('Do you really want to exit?', function (confirmed) {
+        var exit = function () {
+            navigator.app.exitApp();
+        };
+        if (confirmed === true || confirmed === 1) {
+            AppHelper.logout().then(exit, exit);
+        }
+    }, 'Exit', 'Ok,Cancel');
+};
+var onDeviceReady = function() {
+    //Handle document events
+    document.addEventListener("backbutton", onBackKeyDown, false);
+};
 
-    document.addEventListener("deviceready", onDeviceReady, false);
+document.addEventListener("deviceready", onDeviceReady, false);
 
+var mobileApp = new kendo.mobile.Application(document.body, { transition: 'slide', statusBarStyle: "black", layout: 'mobile-tabstrip', skin: 'flat' });
 
 var app = { viewModels: {} };
